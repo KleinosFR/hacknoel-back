e78ecdc84fceeb4a764f08181754ac60834ea6f8
angelique-w <angelique.wons@gmail.com>
2019-12-19 17:11:43 +0100

    created routes, attention : routes delete are not functionnal

--- sequelize/associations.js
+++ sequelize/associations.js
@@ -1,20 +1,18 @@
 const User = require("./models/users");
-const Language = require("./models/language");
+const Language = require("./models/languages");
 const Session = require("./models/sessions");
 const Sign = require("./models/signs");
 
-
-
 // USER
 User.hasMany(Sign, { foreignKey: { allowNull: false }, onDelete: "CASCADE" });
-User.belongsTo(Promo, { foreignKey: { allowNull: false }, onDelete: "CASCADE" });
-User.belongsTo(Language, { foreignKey: { allowNull: false }, onDelete: "CASCADE" });
+User.belongsTo(Session, { foreignKey: { allowNull: false } });
+User.belongsTo(Language, { foreignKey: { allowNull: false } });
 
 // SIGN
-Sign.belongsTo(User, { foreignKey: { allowNull: false }});
+Sign.belongsTo(User, { foreignKey: { allowNull: false } });
 
-// PROMO
-Promo.hasMany(User, { foreignKey: { allowNull: false } });
+// SESSION
+Session.hasMany(User, { foreignKey: { allowNull: false } });
 
 // LANGUAGE
 Language.hasMany(User, { foreignKey: { allowNull: false } });
--- sequelize/models/users.js
+++ sequelize/models/users.js
@@ -2,36 +2,37 @@ const Sequelize = require("sequelize");
 const sequelize = require("../index");
 
 const User = sequelize.define(
-  "User",
-  {
-    uuid: {
-      type: Sequelize.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4
+    "User",
+    {
+        uuid: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4
+        },
+        firstName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        role: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        is_OAuth: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
     },
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    role: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }, 
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    is_OAuth: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false
-    }
-  },
-  {}
+    {}
 );
 
 module.exports = User;
