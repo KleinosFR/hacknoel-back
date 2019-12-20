bd4c3b4b6eed721727fb906fce9a090bb8809efa
lucas <lucasleproust@gmail.com>
2019-12-20 03:14:22 +0100

    add passport-google

--- passport.js
+++ passport.js
@@ -11,17 +11,21 @@ const credentials = {
     github: {
         clientID: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET,
-        callbackURL:
-            "https://hacknoel-api.herokuapp.com/login/auth/github/callback"
+        callbackURL: "https://hacknoel-api.herokuapp.com/login/auth/github/callback"
+    },
+    google: {
+        clientID: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: "https://hacknoel-api.herokuapp.com/login/auth/google/callback"
     }
 };
 
-passport.serializeUser((user, done) => {
-    done(null, user);
+passport.serializeUser((user, cb) => {
+    cb(null, user);
 });
 
-passport.deserializeUser((user, done) => {
-    done(null, user);
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
 passport.use(
@@ -40,6 +44,33 @@ passport.use(
                 },
                 defaults: {
                     isOAuth: false
+                }
+            });
+
+            userData.jwt = jwt.sign({ email: userData.email }, secret, {
+                expiresIn: "1h"
+            });
+
+            cb(null, userData);
+        }
+    )
+);
+
+
+passport.use(
+    new GoogleStrategy(
+        credentials.google,
+        async (accesToken, refreshToken, profile, cb) => {
+            let userData = {
+                email: profile.emails[0].value,
+            };
+
+            await User.findOrCreate({
+                where: {
+                    email: userData.email
+                },
+                defaults: {
+                    isOAuth: true
                 }
             });
 
